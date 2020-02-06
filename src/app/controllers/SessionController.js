df0d3668ffa09fdd72c3e8b8a3bbc830627a68f2
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-02-06 17:42:44 -0300

    validating data create, update user and session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,22 +1,23 @@
 import jwt from 'jsonwebtoken';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
-        // const schema = Yup.object().shape({
-        //     email: Yup.string().email(),
-        //     cnpj: Yup.string(),
-        //     password: Yup.string()
-        //         .required()
-        //         .min(6),
-        // });
+        const schema = Yup.object().shape({
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string().required(),
+        });
 
-        // if (!(await schema.isValid(req.body))) {
-        //     return res.status(400).json({ error: 'Erro de validação' });
-        // }
+        if (!(await schema.isValid(req.body))) {
+            return res
+                .status(400)
+                .json({ error: 'Erro na validação de dados' });
+        }
 
         const { email, password } = req.body;
 
