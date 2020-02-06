7ef1e43e31e45424512557338791b4aeb335287c
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-02-06 11:21:46 -0300

    configuration sequelize and create migration user

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+// require('dotenv/config');
+
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'fastfeet',
+    // host: process.env.DB_HOST,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // port: process.env.DB_PORT,
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
